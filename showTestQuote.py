ebc61692865ffdaed3f5d485e398d3aec5ebd0bd
Rishav Raj <64187401+rishavraj221@users.noreply.github.com>
2020-04-27 12:06:51 +0530

    Create showTestQuote.py

---
+++ showTestQuote.py
@@ -0,0 +1,10 @@
+  
+#!/usr/bin/python
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from windowDisplay import *
+
+wd = windowDisplay()
+wd.initiate()
+
+wd.set('E=MC2 -Albert')
+wd.show()
